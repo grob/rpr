24a6fab0123705b087b50797c1b866e34802dac0
Robert Gaggl <robert@nomatic.org>
2011-12-16 10:59:29 +0100

    index package name as ngrams too

--- index.js
+++ index.js
@@ -4,13 +4,29 @@ var {IndexManager} = require("indexmanager");
 var {Document, Field} = org.apache.lucene.document;
 var {MultiFieldQueryParser} = org.apache.lucene.queryParser;
 var {Version} = org.apache.lucene.util;
-var {PerFieldAnalyzerWrapper} = org.apache.lucene.analysis;
-var {StandardAnalyzer} = org.apache.lucene.analysis.standard;
+var {Analyzer, PerFieldAnalyzerWrapper, LowerCaseFilter} = org.apache.lucene.analysis;
+var {StandardAnalyzer, StandardTokenizer} = org.apache.lucene.analysis.standard;
 var {MatchAllDocsQuery} = org.apache.lucene.search;
+var {NGramTokenFilter} = org.apache.lucene.analysis.ngram;
 
+
+var NGramAnalyzer = exports.NGramAnalyzer = function(minGram, maxGram) {
+    return new Analyzer({
+        "tokenStream": function(fieldName, reader) {
+            var tokenStream = new StandardTokenizer(Version.LUCENE_35, reader);
+            tokenStream = new LowerCaseFilter(Version.LUCENE_35, tokenStream);
+            tokenStream = new NGramTokenFilter(tokenStream, minGram, maxGram);
+            return tokenStream;
+        }
+    });
+};
+
+var analyzer = new PerFieldAnalyzerWrapper(new StandardAnalyzer(Version.LUCENE_35), {
+    "name_ngrams": new NGramAnalyzer(3, 5)
+});
 
 var manager = exports.manager = module.singleton("index", function() {
-    return IndexManager.createIndex(indexDir, "index");
+    return IndexManager.createIndex(indexDir, "index", analyzer);
 });
 
 exports.createDocument = function(pkg) {
@@ -18,6 +34,7 @@ exports.createDocument = function(pkg) {
     var descriptor = JSON.parse(pkg.latestVersion.descriptor);
     doc.add(new Field("id", pkg._id, Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
     doc.add(new Field("name", pkg.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("name_ngrams", pkg.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     doc.add(new Field("description", descriptor.description, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     for each (var keyword in descriptor.keywords) {
         doc.add(new Field("keyword", keyword, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
@@ -35,9 +52,8 @@ exports.createDocument = function(pkg) {
 exports.search = function(q) {
     var query = null;
     if (typeof(q) === "string" && q.length > 0) {
-        var analyzer = new StandardAnalyzer(Version.LUCENE_35);
         var parser = new MultiFieldQueryParser(Version.LUCENE_35,
-                 ["name", "description", "keyword", "author", "maintainer", "contributor"], analyzer);
+                 ["name", "name_ngrams", "description", "keyword", "author", "maintainer", "contributor"], analyzer);
         query = parser.parse(q || "");
     } else {
         query = new MatchAllDocsQuery();
