b7b285c03b49bc1723e2837a7c28e389efcccc69
Robert Gaggl <robert@nomatic.org>
2012-02-24 09:19:54 +0100

    sort by modifytime desc if no query is given

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 var {indexDir} = require("./config");
 var {Index} = require("lucindex");
 var {Package} = require("./model");
-var {Document, Field} = org.apache.lucene.document;
+var {Document, Field, NumericField} = org.apache.lucene.document;
 var {MultiFieldQueryParser} = org.apache.lucene.queryParser;
 var {Version} = org.apache.lucene.util;
 var {Analyzer, PerFieldAnalyzerWrapper, LowerCaseFilter} = org.apache.lucene.analysis;
 var {StandardAnalyzer, StandardTokenizer} = org.apache.lucene.analysis.standard;
-var {MatchAllDocsQuery} = org.apache.lucene.search;
+var {MatchAllDocsQuery, Sort, SortField} = org.apache.lucene.search;
 var {NGramTokenFilter} = org.apache.lucene.analysis.ngram;
 var {Float} = java.lang;
 
@@ -34,25 +34,35 @@ var manager = exports.manager = module.singleton("index", function() {
 var createDocument = exports.createDocument = function(pkg) {
     var doc = new Document();
     var descriptor = JSON.parse(pkg.latestVersion.descriptor);
-    doc.add(new Field("id", pkg._id, Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
-    doc.add(new Field("name", pkg.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
-    doc.add(new Field("name_ngrams", pkg.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
-    doc.add(new Field("description", descriptor.description, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("id", pkg._id,
+            Field.Store.YES, Field.Index.NOT_ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("name", pkg.name,
+            Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("name_ngrams", pkg.name,
+            Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("description", descriptor.description,
+            Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add((new NumericField("modifytime")).setLongValue(Math.abs(pkg.modifytime.getTime() / 1000)));
     for each (var keyword in descriptor.keywords) {
-        doc.add(new Field("keyword", keyword, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+        doc.add(new Field("keyword", keyword,
+                Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     }
-    doc.add(new Field("author", pkg.author.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+    doc.add(new Field("author", pkg.author.name,
+            Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     for each (var maintainer in pkg.maintainers) {
-        doc.add(new Field("maintainer", maintainer.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+        doc.add(new Field("maintainer", maintainer.name,
+                Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     }
     for each (var contributor in pkg.contributors) {
-        doc.add(new Field("contributor", contributor.name, Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
+        doc.add(new Field("contributor", contributor.name,
+                Field.Store.NO, Field.Index.ANALYZED, Field.TermVector.NO));
     }
     return doc;
 };
 
 exports.search = function(q, length, offset) {
     var query = null;
+    var sort = new Sort();
     if (typeof(q) === "string" && q.length > 0) {
         var parser = new MultiFieldQueryParser(Version.LUCENE_35,
                  ["name", "name_ngrams", "description", "keyword", "author", "maintainer", "contributor"],
@@ -63,8 +73,9 @@ exports.search = function(q, length, offset) {
         query = parser.parse(q || "");
     } else {
         query = new MatchAllDocsQuery();
+        sort.setSort(new SortField("modifytime", SortField.LONG, true));
     }
-    var topDocs = manager.searcher.search(query, null, 50);
+    var topDocs = manager.searcher.search(query, null, 50, sort);
     var result = {
         "offset": 0,
         "length": 0,
