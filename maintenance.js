5a48f73dfde58cc3791b5c56b3b1851d878459f5
Robert Gaggl <robert@nomatic.org>
2012-02-26 12:15:28 +0100

    index rebuilding is now done via command line, not via _rebuildIndex action
    added method to populate package owners

---
+++ maintenance.js
@@ -0,0 +1,30 @@
+var {store, Package, Version, User, Author, RelPackageAuthor,
+        RelPackageOwner, LogEntry} = require("./model");
+
+var addOwners = function() {
+    store.beginTransaction();
+    for each (let pkg in Package.all()) {
+        if (pkg.owners.length < 1) {
+            var ownership = RelPackageOwner.create(pkg, pkg.creator, pkg.creator);
+            ownership.save();
+            console.log("Added", pkg.creator.name, "to owners of", pkg.name);
+        }
+    }
+    store.commitTransaction();
+};
+
+var rebuildIndex = function() {
+    console.log("Rebuilding search index...");
+    require("./index").rebuild();
+    console.log("Done");
+};
+
+
+if (require.main == module.id) {
+    var method = system.args.pop();
+    if (!method || typeof(this[method]) !== "function") {
+        console.log("Please specify the method to execute");
+        system.exit(1);
+    }
+    system.exit(this[method](system.args));
+}
