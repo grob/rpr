5235103908fdb18755126bd2182e41fbf042e6f1
Robert Gaggl <robert@nomatic.org>
2013-07-28 11:35:55 +0200

    modified store to switch to in-memory database if environment variable "rpr.mode" is set to "unittest"

--- app/model/store.js
+++ app/model/store.js
@@ -1,16 +1,31 @@
 var {Store, ConnectionPool, Cache} = require("ringo-sqlstore");
 var config = require('../config/config');
+var log = require("ringo/logging").getLogger(module.id);
+
+var isInUnitTestMode = function() {
+    var mode = environment["rpr.mode"];
+    return mode != null && mode.toString() === "unittest";
+};
 
 /**
  * The store this application uses
  * @type {Store}
  */
 var connectionPool = module.singleton("connectionpool", function() {
+    var url = config.db.url;
+    var driver = config.db.driver;
+    var username = config.db.username;
+    var password = config.db.password;
+    if (isInUnitTestMode()) {
+        url = "jdbc:h2:mem:test;MVCC=TRUE";
+        driver = "org.h2.Driver";
+    }
+    log.info("Instantiating connection pool:", username + "@" + url);
     return new ConnectionPool({
-        "url": config.db.url,
-        "driver": config.db.driver,
-        "username": config.db.username,
-        "password": config.db.password
+        "url": url,
+        "driver": driver,
+        "user": username,
+        "password": password
     });
 });
 
