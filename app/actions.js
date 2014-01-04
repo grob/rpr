8fd733e0a225565120ca08b5b89b975caa855293
Robert Gaggl <robert@nomatic.org>
2014-01-04 20:05:42 +0100

    removed old frontend - the new one is http://github.com/grob/rpr-frontend

--- app/actions.js
+++ app/actions.js
@@ -6,20 +6,10 @@ var index = require("./index");
 var app = exports.app = new Application();
 app.configure("gzip", "etag", "requestlog", "error", "notfound", "params",
         "upload", "mount", "route");
-app.configure("static");
-app.static(module.resolve("../static/"), "index.html");
 
 // mount apps
 app.mount("/api", module.resolve("./api/main"));
 app.mount("/download", module.resolve("./download"));
-
-app.get("/search*?", function(request) {
-    return response.static(module.resolve("../static/index.html"));
-});
-
-app.get("/packages*?", function(request) {
-    return response.static(module.resolve("../static/index.html"));
-});
 
 app.get("/_rebuildIndex", function() {
     index.rebuild();
--- static/js/build.js
+++
@@ -1,8 +0,0 @@
-({
-    "baseUrl": "./",
-    "mainConfigFile": "./init.js",
-    "name": "init",
-    "out": "./init.built.js",
-    "keepBuildDir": true,
-    "logLevel": 0
-})
